7448078ef97999439337328d6938cd4845dcfc2b
4LEAF_DEV_MKH <77660455+mkh0169@users.noreply.github.com>
2022-06-16 15:11:14 +0900

    theme

--- src/Shell.jsx
+++ src/Shell.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import styled from "styled-components";
+import styled, {ThemeProvider} from "styled-components";
+import theme from './theme'; 
+
 
 const Title = styled.h2`
   color: ${ (props) => props.co};
@@ -11,16 +13,55 @@ const Title = styled.h2`
   }
 `;
 
+const Button = styled.button`
+  background-color: ${(props) => props.theme.dangerColor};
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato
+`;
+
+const Primary_Button = styled.button`
+  background : ${props => props.primary ? "palevioletred" : "white"};
+  color : ${props => props.primary ? "white" : "palevioletred"};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
 const Shell = () => {
     return (
-    <div>
-        <Title co="#999">Hello Styled</Title>
-        <Title co="#f12">Hello Styled</Title>
-        <Title co="#bb2">Hello Styled</Title>
-        <Title co="#e713">Hello Styled</Title>
-    </div>
+      <ThemeProvider theme={theme}>
+        <div>
+            <Title co="#999">Hello Styled
+              <div>
+                <Primary_Button>normal</Primary_Button>
+               <Primary_Button primary>primary</Primary_Button>
+             </div>
+           </Title>
+           <Title co="#f12">Hello Styled</Title>
+              <div>
+               <Button>Normal Button</Button>
+               <TomatoButton>Tomato Button</TomatoButton>
+             </div>
+           
+           <Title co="blue">Hello Styled</Title>
+             <div>
+              <Button ></Button>
+             </div>
+           <Title co="pink">Hello Styled</Title>
+        </div>
+      </ThemeProvider>
+
     );
 };
 
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+
+const theme = {
+    mainColor: '#3498db',
+    dangerColor: 'blue',
+    successColor: 'pink',
+
+};
+
+export default theme;
